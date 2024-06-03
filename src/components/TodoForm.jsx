31a9c1e4371c1d1f7e1d3069ddf7b5347411c699
vaibh2723 <vaibhavjaiswal2707@gmail.com>
2024-06-03 21:07:47 +0530

    Normalize line endings

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoForm.css'
 
-const TodoForm = () => {
+const TodoForm = ({ setTask }) => {
+
+    const [taskData, setTaskData] = useState({
+        text: '',
+        subText: '',
+        status: 'TODO'
+    })
+
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        setTaskData((prev) => {
+            return { ...prev, [name]: value }
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("this is clicked")
+        setTask((prev) => {
+            return [...prev, taskData]
+        })
+    }
+
+
+
     return (
         <div className='todoFormContainer'>
-            <form className='formContainer'>
-                <input type="text" className='input' placeholder='Enter the Task' />
-                <input type="text" className='input' placeholder='Enter task Description' />
+            <form className='formContainer' onSubmit={handleSubmit}>
+                <input type="text" className='input' name='text' placeholder='Enter the Task' onChange={handleInputChange} />
+                <input type="text" className='input' name='subText' placeholder='Enter task Description' onChange={handleInputChange} />
                 <button type='submit' className='button'> + Add Task</button>
             </form>
-        </div>
+        </div >
     )
 }
 
